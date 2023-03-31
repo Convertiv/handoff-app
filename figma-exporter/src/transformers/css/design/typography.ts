33a3b663502d318ae46ad81c04caacaa4c082808
Domagoj Gojak <domagoj@convertiv.com>
2023-03-31 12:02:51 +0200

    Resolved issue where typography tokens would not include the type group (if any).

--- figma-exporter/src/transformers/css/design/typography.ts
+++ figma-exporter/src/transformers/css/design/typography.ts
@@ -3,24 +3,25 @@ import { TypographyObject } from "../../../types";
 export default function transformTypography(typography: TypographyObject[]): string {
   const stringBuilder: Array<string> = [];
 
-  stringBuilder.push(`$type-sizes: ( ${typography.map(type => (
-    type.group
-      ? `"${type.group}-${type.machine_name}"`
-      : `"${type.machine_name}"`
-  )).join(', ')} );`);
-
+  stringBuilder.push(`$type-sizes: ( ${typography.map(type => `"${getTypeName(type)}"`).join(', ')} );`);
   stringBuilder.push(``);
 
   typography.forEach(type => {
     stringBuilder.push([
-      `--typography-${type.machine_name}-font-family: '${type.values.fontFamily}';`,
-      `--typography-${type.machine_name}-font-size: ${type.values.fontSize}px;`,
-      `--typography-${type.machine_name}-font-weight: ${type.values.fontWeight};`,
-      `--typography-${type.machine_name}-line-height: ${(type.values.lineHeightPx / type.values.fontSize).toFixed(1)};`,
-      `--typography-${type.machine_name}-letter-spacing: ${type.values.letterSpacing}px;`,
-      `--typography-${type.machine_name}-paragraph-spacing: ${type.values.paragraphSpacing | 20}px;`,
+      `--typography-${getTypeName(type)}-font-family: '${type.values.fontFamily}';`,
+      `--typography-${getTypeName(type)}-font-size: ${type.values.fontSize}px;`,
+      `--typography-${getTypeName(type)}-font-weight: ${type.values.fontWeight};`,
+      `--typography-${getTypeName(type)}-line-height: ${(type.values.lineHeightPx / type.values.fontSize).toFixed(1)};`,
+      `--typography-${getTypeName(type)}-letter-spacing: ${type.values.letterSpacing}px;`,
+      `--typography-${getTypeName(type)}-paragraph-spacing: ${type.values.paragraphSpacing | 20}px;`,
     ].join('\n'));
   })
   
   return stringBuilder.join('\n');
+}
+
+function getTypeName(type: TypographyObject): string {
+  return type.group
+    ? `${type.group}-${type.machine_name}`
+    : `${type.machine_name}`;
 }
--- figma-exporter/src/transformers/scss/design/typography.ts
+++ figma-exporter/src/transformers/scss/design/typography.ts
@@ -5,14 +5,20 @@ export default function transformTypography(typography: TypographyObject[]): str
 
   typography.forEach(type => {
     stringBuilder.push([
-      `$typography-${type.machine_name}-font-family: '${type.values.fontFamily}';`,
-      `$typography-${type.machine_name}-font-size: ${type.values.fontSize}px;`,
-      `$typography-${type.machine_name}-font-weight: ${type.values.fontWeight};`,
-      `$typography-${type.machine_name}-line-height: ${(type.values.lineHeightPx / type.values.fontSize).toFixed(1)};`,
-      `$typography-${type.machine_name}-letter-spacing: ${type.values.letterSpacing}px;`,
-      `$typography-${type.machine_name}-paragraph-spacing: ${type.values.paragraphSpacing | 20}px;`,
+      `$typography-${getTypeName(type)}-font-family: '${type.values.fontFamily}';`,
+      `$typography-${getTypeName(type)}-font-size: ${type.values.fontSize}px;`,
+      `$typography-${getTypeName(type)}-font-weight: ${type.values.fontWeight};`,
+      `$typography-${getTypeName(type)}-line-height: ${(type.values.lineHeightPx / type.values.fontSize).toFixed(1)};`,
+      `$typography-${getTypeName(type)}-letter-spacing: ${type.values.letterSpacing}px;`,
+      `$typography-${getTypeName(type)}-paragraph-spacing: ${type.values.paragraphSpacing | 20}px;`,
     ].join('\n'))
   })
   
   return stringBuilder.join('\n');
+}
+
+function getTypeName(type: TypographyObject): string {
+  return type.group
+    ? `${type.group}-${type.machine_name}`
+    : `${type.machine_name}`;
 }
