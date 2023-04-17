c5d311f035d82ecb6767a1472a6bde5ae68a53fc
Brad Mering <brad@convertiv.com>
2023-04-17 08:50:39 -0600

    Fixing size map bug

--- figma-exporter/src/utils/config.ts
+++ figma-exporter/src/utils/config.ts
@@ -37,7 +37,7 @@ export const mapComponentSize = (figma: string, component?: string): string => {
       return componentSize?.css;
     }
   }
-  const coreMap = config.components.size as ComponentSizeMap[];
+  const coreMap = config.figma.size as ComponentSizeMap[];
   const size = coreMap.find((size) => size.figma === figma);
-  return size?.css ?? 'sm';
+  return size?.css ?? figma;
 };
