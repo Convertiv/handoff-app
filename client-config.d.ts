944418eeba5844942c1433cc72d782a6db4277cd
Brad Mering <brad@convertiv.com>
2023-05-31 20:16:06 -0600

    adding type export for plugins

--- client-config.d.ts
+++ client-config.d.ts
@@ -1,5 +1,7 @@
 import type { Config } from './figma-exporter/src/config';
+import type { PluginTransformer } from './figma-exporter/src/transformers/plugin';
 export type HandoffConfig = Config;
 declare const config: Config;
 export type Config = Config;
+export type Plugin = PluginTransformer;
 export default config;
