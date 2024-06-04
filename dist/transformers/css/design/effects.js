318e790accfdb9eb9b3dc52747a9c8957d718962
Domagoj Gojak <domagoj@convertiv.com>
2024-06-04 14:09:35 +0200

    Fixed indentations for foundation (colors, effects, typography) css files.

--- dist/transformers/css/design/effects.js
+++ dist/transformers/css/design/effects.js
@@ -10,7 +10,7 @@ function transformEffects(effects) {
     var validEffects = effects === null || effects === void 0 ? void 0 : effects.filter(function (effect) { return effect.effects && effect.effects.length > 0; });
     if (validEffects) {
         validEffects.forEach(function (effect) {
-            stringBuilder.push("  --effect-".concat(effect.group, "-").concat(effect.machineName, ": ").concat(effect.effects.map(function (effect) { return effect.value; }).join(', ') || 'none', ";"));
+            stringBuilder.push("\t--effect-".concat(effect.group, "-").concat(effect.machineName, ": ").concat(effect.effects.map(function (effect) { return effect.value; }).join(', ') || 'none', ";"));
         });
     }
     return ":root {\n".concat(stringBuilder.join('\n'), "\n}\n");
