f7e7751c76ce33451582590ece3aed564f1d356b
Domagoj Gojak <domagoj@convertiv.com>
2024-06-04 14:08:51 +0200

    Introduced css number value normalization.

---
+++ dist/utils/numbers.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.normalizeCssNumber = void 0;
+var normalizeCssNumber = function (input) {
+    if (input % 1 === 0) {
+        return input.toString();
+    }
+    var rounded = parseFloat(input.toFixed(2));
+    var roundedStr = rounded.toFixed(2);
+    if (rounded === 0) {
+        return '0';
+    }
+    // Remove trailing zeros
+    roundedStr = parseFloat(roundedStr).toString();
+    if (Math.abs(rounded) < 1) {
+        return rounded < 0 ? '-' + roundedStr.slice(2) : roundedStr.slice(1);
+    }
+    return roundedStr;
+};
+exports.normalizeCssNumber = normalizeCssNumber;
---
+++ src/utils/numbers.ts
@@ -0,0 +1,21 @@
+export const normalizeCssNumber = (input: number): string => {
+  if (input % 1 === 0) {
+    return input.toString();
+  }
+
+  let rounded = parseFloat(input.toFixed(2));
+  let roundedStr = rounded.toFixed(2);
+
+  if (rounded === 0) {
+    return '0';
+  }
+
+  // Remove trailing zeros
+  roundedStr = parseFloat(roundedStr).toString();
+
+  if (Math.abs(rounded) < 1) {
+    return rounded < 0 ? '-' + roundedStr.slice(2) : roundedStr.slice(1);
+  }
+
+  return roundedStr;
+};
