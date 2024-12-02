96ebb969013fbaa7e341c3cefca3b724049225a8
Brad Mering <brad@convertiv.com>
2024-12-01 19:10:45 -0700

    Fixing component path fetch

--- src/app/components/ComponentPreview.tsx
+++ src/app/components/ComponentPreview.tsx
@@ -146,7 +146,7 @@ export const ComponentDisplay: React.FC<{
               minWidth: width,
               height: height,
             }}
-            src={`/api/components/${previewUrl}`}
+            src={`/api/component/${previewUrl}`}
           />
         </>
       ) : (
