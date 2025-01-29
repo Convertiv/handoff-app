c85d05a0a4b659fb52b137a0aa5797bca0f50f98
Natko <natko@convertiv.com>
2025-01-29 18:06:03 +0100

    Added container query tailwind plugin.

--- src/app/tailwind.config.ts
+++ src/app/tailwind.config.ts
@@ -70,5 +70,5 @@ export default {
       },
     },
   },
-  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
+  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography'), require('@tailwindcss/container-queries')],
 };
