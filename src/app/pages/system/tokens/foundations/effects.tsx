d76dc581211c2619155715b013effe5ac73dc43d
Natko <natko@convertiv.com>
2025-02-07 15:11:19 +0100

    Added AnchorNav to the effects tokens page.

--- src/app/pages/system/tokens/foundations/effects.tsx
+++ src/app/pages/system/tokens/foundations/effects.tsx
@@ -6,8 +6,9 @@ import ReactMarkdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
 import Layout from '../../../../components/Layout/Main';
 import { MarkdownComponents } from '../../../../components/Markdown/MarkdownComponents';
+import AnchorNav from '../../../../components/Navigation/AnchorNav';
 import HeadersType from '../../../../components/Typography/Headers';
-import { Table, TableBody, TableCell, TableRow } from '../../../../components/ui/table';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../../../../components/ui/table';
 import { fetchComponents, fetchDocPageMarkdown, FoundationDocumentationProps, getTokens } from '../../../../components/util';
 
 /**
@@ -59,19 +60,26 @@ const ComponentsPage = ({ content, menu, metadata, current, config, design }: Fo
         <HeadersType.H1>{metadata.title}</HeadersType.H1>
         <p className="text-lg leading-relaxed text-gray-600 dark:text-gray-300">{metadata.description}</p>
       </div>
-      <div className="mt-10">
-        <ReactMarkdown className="prose" components={MarkdownComponents} rehypePlugins={[rehypeRaw]}>
-          {content}
-        </ReactMarkdown>
+      <div className="mt-10 lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_280px]">
+        <div>
+          <ReactMarkdown className="prose" components={MarkdownComponents} rehypePlugins={[rehypeRaw]}>
+            {content}
+          </ReactMarkdown>
 
-        {Object.keys(effectGroups).map((group) => (
-          <div key={group} className="mb-8">
-            <h3 className="mb-4 text-lg font-medium" id={`${group}-colors`}>
-              {upperFirst(group)}
-            </h3>
-            <EffectsTable group={group} effects={effectGroups[group]} />
-          </div>
-        ))}
+          {Object.keys(effectGroups).map((group) => (
+            <div key={group} className="mb-8">
+              <h3 className="mb-4 text-lg font-medium" id={`${group}-colors`}>
+                {upperFirst(group)}
+              </h3>
+              <EffectsTable group={group} effects={effectGroups[group]} />
+            </div>
+          ))}
+        </div>
+        <AnchorNav
+          groups={[
+            Object.assign({}, ...[...Object.keys(effectGroups).map((group) => ({ [`${group}-effects`]: `${upperFirst(group)} Effects` }))]),
+          ]}
+        />
       </div>
     </Layout>
   );
@@ -79,12 +87,20 @@ const ComponentsPage = ({ content, menu, metadata, current, config, design }: Fo
 
 const EffectsTable = ({ group, effects }: { group: string; effects: EffectObject[] }) => {
   return (
-    <Table>
+    <Table className="border-b-[0.5px] border-l-[0.5px] border-r-[0.5px]">
+      <TableHeader className="border-b-0 border-l-[0.5px] border-r-[0.5px] border-t-[0.5px] bg-gray-50/80 dark:bg-gray-800/80 ">
+        <TableRow className="!border-b-[0.5px]">
+          <TableHead className="border-r-[0.5px] px-4 text-xs font-light text-gray-900 dark:text-gray-100">Reference</TableHead>
+          <TableHead className="border-r-[0.5px] px-4 text-xs font-light text-gray-900 dark:text-gray-100">Effects</TableHead>
+        </TableRow>
+      </TableHeader>
       <TableBody>
         {effects.map((effect) => (
-          <TableRow key={effect.id} className="h-12">
-            <TableCell>{effect.reference}</TableCell>
-            <TableCell className="text-right">{effect.effects.map((effect) => effect.value).join(', ') || 'none'}</TableCell>
+          <TableRow key={effect.id} className="h-10 border-b-[0.5px]">
+            <TableCell className="border-r-[0.5px] px-3.5 py-1">{effect.reference}</TableCell>
+            <TableCell className="border-r-[0.5px] px-3.5 py-1">
+              {effect.effects.map((effect) => effect.value).join(', ') || 'none'}
+            </TableCell>
           </TableRow>
         ))}
       </TableBody>
