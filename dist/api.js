e58969f34ae4332b966b301be2a5c7175f85e6bf
Domagoj Gojak <domagoj@convertiv.com>
2025-11-30 10:25:19 +0100

    Remove changelog functionality and related files from the project, including API references and documentation components.

--- dist/api.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateChangelogRecord = void 0;
-var changelog_1 = require("./changelog");
-Object.defineProperty(exports, "generateChangelogRecord", { enumerable: true, get: function () { return __importDefault(changelog_1).default; } });
